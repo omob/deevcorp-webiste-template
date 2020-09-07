1183984331493e0858ab6abcae51e4f3b5d4bad5
Ayodeji Abodunrin <aboayosam@gmail.com>
2020-09-07 12:47:22 +0100

    working on project-template

--- src/templates/project-template.js
+++ src/templates/project-template.js
@@ -1,10 +1,26 @@
 import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
+import styled from "styled-components"
 
-const ProjectTemplate = ({ data }) => {
-  console.log(data)
-  return <Layout>|</Layout>
+const HeaderBanner = styled.section`
+  height: 400px;
+  background-color: red;
+`
+const Content = styled.section``
+
+const ProjectTemplate = ({ data: { project: project } }) => {
+  console.log(project)
+  return (
+    <Layout>
+      <HeaderBanner>
+        <h1>{project.title}</h1>
+      </HeaderBanner>
+      <Content>
+        <pre>{project}</pre>
+      </Content>
+    </Layout>
+  )
 }
 
 export const query = graphql`
