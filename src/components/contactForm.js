59f61c01881317f1b5cd3d782a85c3cffa93e300
Ayodeji Abodunrin <aboayosam@gmail.com>
2021-01-09 11:30:39 +0100

    Fixing netfligy form not working error

--- src/components/contactForm.js
+++ src/components/contactForm.js
@@ -35,70 +35,41 @@ const FormWrapper = styled.div`
 `;
 function ContactForm(props) {
     return (
-      // <FormWrapper>
-      //   <form
-      //     name="contact"
-      //     netlify
-      //   >
-      //     <input type="hidden" name="form-name" value="contact" />
-      //     <p>
-      //       <input
-      //         className="form-control"
-      //         type="text"
-      //         name="name"
-      //         placeholder="Your Name"
-      //       />
-      //     </p>
-      //     <p>
-      //       <input
-      //         className="form-control"
-      //         type="email"
-      //         name="email"
-      //         placeholder="Your Email"
-      //       />
-      //     </p>
-      //     <p>
-      //       <textarea
-      //         rows="5"
-      //         className="form-control"
-      //         name="message"
-      //         placeholder="Enter Message"
-      //       ></textarea>
-      //     </p>
-      //     <p style={{ textAlign: "right" }}>
-      //       <Button style={{ width: "150px" }} title="Send" type="submit" />
-      //     </p>
-      //   </form>
-      // </FormWrapper>
-      <form name="contact" method="POST" data-netlify="true">
-        <p>
-          <label>
-            Your Name: <input type="text" name="name" />
-          </label>
-        </p>
-        <p>
-          <label>
-            Your Email: <input type="email" name="email" />
-          </label>
-        </p>
-        <p>
-          <label>
-            Your Role:{" "}
-            <select name="role[]" multiple>
-              <option value="leader">Leader</option>
-              <option value="follower">Follower</option>
-            </select>
-          </label>
-        </p>
-        <p>
-          <label>
-            Message: <textarea name="message"></textarea>
-          </label>
-        </p>
-        <p>
-          <button type="submit">Send</button>
-        </p>
-      </form>
+      <FormWrapper>
+        <form
+          name="contact"
+          netlify
+        >
+          <input type="hidden" name="form-name" value="contact" />
+          <p>
+            <input
+              className="form-control"
+              type="text"
+              name="name"
+              placeholder="Your Name"
+            />
+          </p>
+          <p>
+            <input
+              className="form-control"
+              type="email"
+              name="email"
+              placeholder="Your Email"
+            />
+          </p>
+          <p>
+            <textarea
+              rows="5"
+              className="form-control"
+              name="message"
+              placeholder="Enter Message"
+            ></textarea>
+          </p>
+          <p style={{ textAlign: "right" }}>
+            <Button style={{ width: "150px" }} title="Send" type="submit" />
+          </p>
+        </form>
+      </FormWrapper>
     );
 }
 
