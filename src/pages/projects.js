c18c969aa57d225deda4f24afac3897c01c05074
Ayodeji Abodunrin <aboayosam@gmail.com>
2021-02-03 18:56:54 +0100

    updated projects page layout design

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,4 +1,4 @@
-import { graphql, useStaticQuery } from "gatsby";
+import { graphql, Link, useStaticQuery } from "gatsby";
 import Img from "gatsby-image";
 import { Masonry } from "masonic";
 import React from "react";
@@ -9,6 +9,7 @@ import SEO from "../components/seo";
 
 
   const HeadingSection = styled(SectionWrapper)`
+    padding-top: 1em;
   `;
 
   const HeadingText = styled.h2`
@@ -17,6 +18,18 @@ import SEO from "../components/seo";
     margin: auto;
     font-weight: normal;
     line-height: 1.3em;
+    font-weight: bold;
+
+    span {
+      font-style: italic;
+      background-color: ${({ theme }) => theme.text};
+      color: ${({ theme }) => theme.bodyBg};
+      transition: 0.2s ease-in;
+    }
+    span:hover {
+      background-color: ${({ theme }) => theme.bodyBg};
+      color: gray;
+    }
 
     @media (min-width: 550px) {
       & {
@@ -26,23 +39,44 @@ import SEO from "../components/seo";
     }
     @media (min-width: 992px) {
       & {
-        font-size: 4em;
+        font-size: 3.5em;
+        max-width: 80%;
       }
     }
     @media (min-width: 1500px) {
       & {
-        font-size: 5em;
+        font-size: 4em;
       }
     }
-
   `;
 
   const PortfolioWrapper = styled.div`
-    padding: 20px;
+    padding: 5%;
     width: 100%;
-    max-width: 2080px;
-    margin: 20px auto 0;
-    box-sizing: border-box;
+    max-width: 1400px;
+    margin: auto;
+  `;
+
+  const Project = styled.div`
+    height: auto;
+    padding: 20px;
+    background-color: ${({ theme }) => theme.bodyBg};
+    a {
+      color: inherit;
+      text-decoration: none;
+    }
+
+    h4 {
+      font-size: 1.5em;
+      margin-top: 20px;
+      margin-bottom: 10px;
+    }
+
+    span {
+      letter-spacing: 2px;
+      text-transform: uppercase;
+      font-size: 16px;
+    }
   `;
 
 const MasonryCard = ({
@@ -55,11 +89,13 @@ const MasonryCard = ({
   },
   width,
 }) => (
-  <div style={{position: "relative"}} key={index}>
-    <Img fluid={image} alt={title} />
-    <h4>{title}</h4>
-    <h4>{type}</h4>
-  </div>
+  <Project key={index}>
+    <Link to={`${slug}`}>
+      <Img fluid={image} alt={title} />
+      <h4>{title}</h4>
+      <span>{type}</span>
+    </Link>
+  </Project>
 );
 
 const Projects = () => {
@@ -91,12 +127,18 @@ const Projects = () => {
       <SEO title="Projects" />
       <HeadingSection style={{ marginTop: "4em" }}>
         <HeadingText>
-          Our portfolio range from Web Design to Application Development to
-          UI/UX
+          Our Portfolio range from <span>Web Design</span> to{" "}
+          <span>Application </span> Development to <span>UI/UX</span>
         </HeadingText>
       </HeadingSection>
       <PortfolioWrapper>
-        <Masonry items={projectsList} columnGutter={20} overscanBy={5} columnWidth={300} render={MasonryCard} />
+        <Masonry
+          items={projectsList}
+          columnGutter={10}
+          overscanBy={1}
+          columnWidth={320}
+          render={MasonryCard}
+        />
       </PortfolioWrapper>
     </Layout>
   );
